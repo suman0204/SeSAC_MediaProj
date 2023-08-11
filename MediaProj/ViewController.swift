4ec0193af718e359809854a0e0d57c4517c1f524
sumanhong <suman0204@naver.com>
2023-08-11 17:56:57 +0900

    Initial Commit

---
+++ MediaProj/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MediaProj
+//
+//  Created by 홍수만 on 2023/08/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
